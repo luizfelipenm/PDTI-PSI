7ce0acf3302405f6a1c8817471b63c600398c551
Luiz Felipe <111882138+luizfelipenm@users.noreply.github.com>
2024-06-26 13:40:23 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,12 @@ const botoesVisualizar = document.querySelectorAll('.visualizar');
 const pdfEmbed = document.getElementById('pdf-embed');
 
 botoesVisualizar.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const pdfCaminho = botao.getAttribute('data-pdf');
-        pdfEmbed.src = pdfCaminho; 
-    });
+  botao.addEventListener('click', () => {
+    const pdfCaminho = botao.getAttribute('data-pdf');
+    pdfEmbed.src = pdfCaminho;
+
+    botoesVisualizar.forEach(btn => btn.classList.remove('active')); 
+    botao.classList.add('active'); 
+  });
 });
+
